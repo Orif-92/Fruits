904d89e9f0cce89bb12533b7b20765a695fa1870
Orif-92 <sarniyozovorif@gmail.com>
2022-06-30 12:41:02 +0300

    project files been uploaded

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+# title
+st.title('Olma, Lemon, Mango mevalarini klassifikasiya qiluvchi model')
+
+# rasmni joylash
+file = st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'svg'])
+st.image(file)
+# PIL convert
+img = PILImage.create(file)
+
+# model
+model = load_learner('fruit_model.pkl')
+
+# prediction
+pred, pred_id, probs = model.predict(img)
+st.success(f"Bashorat: {pred}")
+st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}')
+
+# plotting
+fig = px.bar(x=probs*100, y=model.vocab)
+st.plotly_chart(fig)
