264e83cdea6e95b67281bead8c8f223b4cf31da2
Orif Sarniyozov <60812094+Orif-92@users.noreply.github.com>
2022-07-02 08:34:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,19 @@ st.title('Olma, Lemon, Mango mevalarini klassifikasiya qiluvchi model')
 
 # rasmni joylash
 file = st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'svg'])
-st.image(file)
-# PIL convert
-img = PILImage.create(file)
+if fale:
+  st.image(file)
+  # PIL convert
+  img = PILImage.create(file)
 
-# model
-model = load_learner('fruit_model.pkl')
+  # model
+  model = load_learner('fruit_model.pkl')
 
-# prediction
-pred, pred_id, probs = model.predict(img)
-st.success(f"Bashorat: {pred}")
-st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}')
+  # prediction
+  pred, pred_id, probs = model.predict(img)
+  st.success(f"Bashorat: {pred}")
+  st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}')
 
-# plotting
-fig = px.bar(x=probs*100, y=model.vocab)
-st.plotly_chart(fig)
+  # plotting
+  fig = px.bar(x=probs*100, y=model.vocab)
+  st.plotly_chart(fig)
