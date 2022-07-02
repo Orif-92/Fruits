b91734423262ec758c4836f5379f13c051ecaa7c
Orif Sarniyozov <60812094+Orif-92@users.noreply.github.com>
2022-07-02 08:27:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
 import streamlit as st
 from fastai.vision.all import *
 import pathlib
-#import plotly.express as px
-#temp = pathlib.PosixPath
-#pathlib.PosixPath = pathlib.WindowsPat
-import platform
-
-plt = platform.sytem()
-if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
 # title
 st.title('Olma, Lemon, Mango mevalarini klassifikasiya qiluvchi model')
 
 # rasmni joylash
 file = st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'svg'])
-if file:
-    st.image(file)
-    # PIL convert
-    img = PILImage.create(file)
-    # model
-    model = load_learner('fruit_model.pkl')
-    # prediction
-    pred, pred_id, probs=model.predict(img)
-    st.success(f"Bashorat: {pred}")
-    st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}%')
-    # plotting
-   #fig=px.bar(x=probs*100, y=model.vocab)
-   #st.plotly_chart(fig)
+st.image(file)
+# PIL convert
+img = PILImage.create(file)
+
+# model
+model = load_learner('fruit_model.pkl')
+
+# prediction
+pred, pred_id, probs = model.predict(img)
+st.success(f"Bashorat: {pred}")
+st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}')
+
+# plotting
+fig = px.bar(x=probs*100, y=model.vocab)
+st.plotly_chart(fig)
